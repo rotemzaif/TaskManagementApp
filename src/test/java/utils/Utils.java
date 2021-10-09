e06a63a88b9a8e8a18e2d756e47be8d0fb3950e7
rotem <rotem8zaif@gmail.com>
2021-10-09 10:42:19 +0300

    added excel read support methods

--- src/test/java/utils/Utils.java
+++ src/test/java/utils/Utils.java
@@ -1,5 +1,10 @@
 package utils;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,6 +16,10 @@ import java.util.Properties;
 public class Utils {
     // objects
     private static FileInputStream fis;
+    private static HSSFWorkbook ExcelWBook;
+    private static HSSFSheet ExcelWSheet;
+    private static HSSFCell Cell;
+
     // variables
 
     // methods
@@ -32,5 +41,44 @@ public class Utils {
             e.printStackTrace();
         }
         return value;
+    }
+
+    public static Object[][] getDataFromExcel(String filePath, String sheetName) {
+        Object[][] data = null;
+        try {
+            fis = new FileInputStream(filePath);
+            // Access the required test data sheet
+            ExcelWBook = new HSSFWorkbook(fis);
+            ExcelWSheet = ExcelWBook.getSheet(sheetName);
+            int startRow = 1;
+            int startCol = 0;
+            int totRows = ExcelWSheet.getLastRowNum();
+            org.apache.poi.ss.usermodel.Row r = ExcelWSheet.getRow(0);
+            int totCols = r.getLastCellNum();
+            data = new Object[totRows][totCols];
+            int di = 0;
+            for (int i = startRow; i <= totRows; di++,i++) {
+                int dj = 0;
+                for (int j = startCol; j < totCols; dj++,j++) {
+                    data[di][dj] = getCellValue(i,j);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not read the Excel sheet");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Could not read the Excel sheet");
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    public static String getCellValue(int rowNum, int colNUm){
+        Cell = ExcelWSheet.getRow(rowNum).getCell(colNUm);
+        Cell.setCellType(CellType.STRING);
+        CellType cellType = Cell.getCellType();
+        if(cellType == CellType.BLANK)
+            return "";
+        else return Cell.getStringCellValue();
     }
 }
