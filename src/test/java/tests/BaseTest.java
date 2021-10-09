c782622f361d1ebfbdc8115079bca3f5a4505cb3
rotem <rotem8zaif@gmail.com>
2021-10-09 10:18:45 +0300

    created and added BaseTest class which consists of @Before and @After class methods

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,58 @@
+package tests;
+
+import io.github.bonigarcia.wdm.config.DriverManagerType;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import pageObjects.TasksPage;
+import utils.Utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    WebDriver driver;
+    TasksPage tp;
+    @BeforeClass
+    public void setup() throws InterruptedException {
+        ChromeDriverManager.getInstance(DriverManagerType.CHROME).setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS) ;
+        driver.get(Utils.readProperty("homePageURL"));
+        tp = new TasksPage(driver);
+        Thread.sleep(500);
+        String expectedPageTitle = tp.getFirstTabName() + " " + Utils.readProperty("pageTitle");
+        String actualPageTitle = driver.getTitle();
+        Assert.assertEquals(actualPageTitle, expectedPageTitle, "'" + expectedPageTitle + "' page is not displayed");
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @AfterMethod
+    public void failedTest(ITestResult result) {
+        //check if the test failed
+        if (result.getStatus() == ITestResult.FAILURE ){
+            TakesScreenshot ts = (TakesScreenshot)driver;
+            File srcFile = ts.getScreenshotAs(OutputType.FILE);
+            try {
+                //result.getname() method will return current test case name.
+                FileUtils.copyFile(srcFile, new File("./ScreenShots/" +result.getName() + ".jpg"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
