9e971526d0a319ebabefc0e75df57905d607e5cb
rotem <rotem8zaif@gmail.com>
2021-10-29 11:17:30 +0300

    test file rafactor: merging validation into one test method, added tab sort display test method, added show-completed-task display test method

--- src/test/java/tests/TabActionsTest.java
+++ src/test/java/tests/TabActionsTest.java
@@ -12,93 +12,89 @@ public class TabActionsTest extends BaseTest {
     String tabRaname = "rzf - tab rename test";
     int numOfTabBefore = 0;
 
-    @Test(description = "cancel create new tab and verify num of tabs in the visible tab list stays the same")
+    @Test(description = "cancel create new tab and verify num of visible tabs and num of tabs in TabSelectList has not changed")
     public void tc01_cancel_tab_creation(){
         tp = new TasksPage(driver);
-        // getting num of tabs before the action
+        // getting num of visible tabs before the action
         numOfTabBefore = tp.getTabList().size();
         tabName = "rzf - new tab test";
-        tp.createNewTab(tabName,"cancel");
-        // getting num of tabs after the action
+        tp.createNewTab(tabName, TasksPage.AlertState.CANCEL);
+        // getting num of visible tabs after the action
         int numOfTabsAfter = tp.getTabList().size();
-        Assert.assertEquals(numOfTabsAfter, numOfTabBefore, "check if tabs was created although canceled");
-    }
-
-    @Test(description = "verify that num of tabs in the 'Select list' menu has not changed")
-    public void tc02_cancel_tab_creation_verify_num_of_tabs_in_Select_menu(){
-        tp = new TasksPage(driver);
         int actualNumOfTabsInSelectList = tp.getTabsSelectList().size()-2;
-        Assert.assertEquals(actualNumOfTabsInSelectList, numOfTabBefore, "check if tab was added in the Select list menu");
+        Assert.assertEquals(numOfTabsAfter, numOfTabBefore, "check if tab was created although tab creation action was canceled");
+        Assert.assertEquals(actualNumOfTabsInSelectList, numOfTabBefore, "check if tab was added in the Select list menu although tab creation action was canceled");
     }
 
-    @Test(description = "create a new tab and verify it was created and added in the visible tab list")
-    public void tc03_create_new_tab(){
+    @Test(description = "create a new tab and verify it was created and added in the visible tab list and in the TabSelectList")
+    public void tc02_create_new_tab(){
         tp = new TasksPage(driver);
-        tabId = tp.createNewTab(tabName, "accept");
+        tabId = tp.createNewTab(tabName, TasksPage.AlertState.ACCEPT);
         tp = new TasksPage(driver);
         Assert.assertTrue(tp.isTabExistInVisibleList(tabId), "the tab was not created or added to the visible tab list");
-    }
-
-    @Test(description = "verify that the new tab was added to the'Select list' menu")
-    public void tc04_verify_new_tab_in_Select_list_menu(){
-        tp = new TasksPage(driver);
         Assert.assertTrue(tp.isTabExistInSelectList(tabId), "the new tab was not added in the 'Select list' menu");
     }
 
     @Test(description = "cancel tab rename action and verify tab name didn't change")
-    public void tc05_rename_tab_cancel(){
+    public void tc03_rename_tab_cancel(){
         tp = new TasksPage(driver);
-        tp.renameTab(tabId, "cancel", tabRaname);
+        tp.renameTab(tabId, TasksPage.AlertState.CANCEL, tabRaname);
         String actualTabName = tp.getTabNameById(tabId);
         Assert.assertEquals(actualTabName, tabName);
     }
 
     @Test(description = "cancel tab rename action and verify tab name didn't change")
-    public void tc06_rename_tab_accept(){
+    public void tc04_rename_tab_accept(){
         tp = new TasksPage(driver);
-        tp.renameTab(tabId, "accept", tabRaname);
+        tp.renameTab(tabId, TasksPage.AlertState.ACCEPT, tabRaname);
         String actualTabName = tp.getTabNameById(tabId);
         Assert.assertEquals(actualTabName, tabRaname);
     }
 
-    @Test(description = "select the new tab, open the action list, select 'Hide list' and verify that the tab is not visible")
-    public void tc07_hide_tab(){
+    @Test(description = "select the new tab, open the action list, select 'Hide list' and verify that the tab is not visible\n")
+    public void tc05_hide_tab(){
         tp = new TasksPage(driver);
         tp.hideTab(tabId);
-        Assert.assertFalse(tp.isTabVisible(tabId), "tab id: " + tabId + "\tname: " + tabRaname + " is still displayed");
+        Assert.assertFalse(tp.isTabVisible(tabId), "tab id: " + tabId + "\tname: " + tabRaname + " is still displayed\n");
     }
 
-    @Test(description = "open the 'Select list' menu, click on the requested tab and verify it is displayed in the visible tab list")
-    public void tc08_re_show_tab(){
+    @Test(description = "open the 'Select list' menu, click on the requested tab and verify it is displayed in the visible tab list\n")
+    public void tc06_show_tab(){
         tp = new TasksPage(driver);
         tp.goToTabFromList(tabId);
-        Assert.assertTrue(tp.isTabVisible(tabId), "tab id: " + tabId + "\tname: " + tabRaname + " is not displayed in the visible tab list");
+        Assert.assertTrue(tp.isTabVisible(tabId), "tab id: " + tabId + "\tname: " + tabRaname + " is not displayed in the visible tab list\n");
     }
 
-    @Test(description = "cancel tab delete action")
+    @Test
+    public void tc07_tab_display_sort_by_priority(){
+        tp = new TasksPage(driver);
+        String sortOption = "Sort by priority";
+        tp.setTabSortDisplay(tabId, sortOption);
+        String actualSortOption = tp.getTabSortOption(tabId);
+        Assert.assertEquals(actualSortOption, sortOption, "'" + sortOption + "' option is not selected\n");
+    }
+
+    @Test
+    public void tc08_tab_display_unselect_Show_completed_tasks(){
+        tp = new TasksPage(driver);
+        tp.setTabCompletedTasksDisplay(tabId, TasksPage.OptionState.UNSELECT);
+        if(tp.isTabCompletedTasksChecked(tabId)) // if 'Show completed tasks' is checked --> test failed
+            Assert.fail("'Show completed tasks' option is still selected/checked");
+    }
+
+    @Test(description = "cancel tab delete action and verify tab was not deleted from visible list and tab select list")
     public void tc09_cancel_delete_tab(){
         tp = new TasksPage(driver);
-        tp.deleteTabById(tabId, "cancel");
-        Assert.assertTrue(tp.isTabExistInVisibleList(tabId), "tab was deleted although delete action was canceled");
+        tp.deleteTabById(tabId, TasksPage.AlertState.CANCEL);
+        Assert.assertTrue(tp.isTabExistInVisibleList(tabId), "tab was deleted from visible tab list although delete action was canceled");
+        Assert.assertTrue(tp.isTabExistInSelectList(tabId), "tab was deleted from the 'Select list' menu although delete action was canceled");
     }
 
-    @Test(description = "verify tab was not deleted from the 'Select list'")
-    public void tc10_cancel_delete_tab_verify_tab_in_Select_list(){
+    @Test(description = "accept delete tab action and verify tab doesn't exist in visible tab list and in the tab Select list")
+    public void tc10_accept_delete_tab(){
         tp = new TasksPage(driver);
-        Assert.assertTrue(tp.isTabExistInSelectList(tabId), "tab was deleted from the 'Select list' menu");
-    }
-
-
-    @Test(description = "accept delete tab action and verify tab doesn't exist in visible tab list")
-    public void tc11_accept_delete_tab(){
-        tp = new TasksPage(driver);
-        tp.deleteTabById(tabId, "accept");
-        Assert.assertFalse(tp.isTabExistInVisibleList(tabId), "tab was not deleted although accepted delete action");
-    }
-
-    @Test(description = "verify that tab doesn't exist in 'Select list' menu")
-    public void tc12_accept_delete_tab(){
-        tp = new TasksPage(driver);
+        tp.deleteTabById(tabId, TasksPage.AlertState.ACCEPT);
+        Assert.assertFalse(tp.isTabExistInVisibleList(tabId), "tab was not deleted from visible list although accepted delete action");
         Assert.assertFalse(tp.isTabExistInSelectList(tabId), "tab was not deleted from 'Select list' menu although accepted delete action");
     }
 }
