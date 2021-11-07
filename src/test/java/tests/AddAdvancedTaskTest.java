d0ec26985c2fbc8da59460cca71d049039e0400e
rotem <rotem8zaif@gmail.com>
2021-11-07 10:19:29 +0200

    page objects instances rename

--- src/test/java/tests/AddAdvancedTaskTest.java
+++ src/test/java/tests/AddAdvancedTaskTest.java
@@ -6,12 +6,10 @@ import org.testng.annotations.Test;
 import pageObjects.AdvancedTaskPage;
 import pageObjects.SettingsPage;
 import pageObjects.Task;
-import pageObjects.TasksPage;
-import utils.DateAnalysis;
+import pageObjects.TaskList;
 import utils.Utils;
 
 import java.text.ParseException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -32,8 +30,8 @@ public class AddAdvancedTaskTest extends BaseTest {
     @Test(description = "check if 'rzf - tasks' tab exists, if there are multiple tabs with that name, delete all except the first;" +
             "if there are no tabs with that name, create a new tab with that name and set its sort option and un-select 'Show completed tasks")
     public void tc01_check_for_existing_testing_tab() {
-        tp = new TasksPage(driver);
-        List<String> tabsWithSameName = tp.getTabIdListForName(tabName, TasksPage.SearchType.EQUAL);
+        tl = new TaskList(driver);
+        List<String> tabsWithSameName = tl.getTabIdListForName(tabName, TaskList.SearchType.EQUAL);
         // if there is one tab with tabName, get its id
         if(tabsWithSameName.size() == 1)
             tabId = tabsWithSameName.get(0);
@@ -42,9 +40,9 @@ public class AddAdvancedTaskTest extends BaseTest {
             String id;
             for (int i = tabsWithSameName.size()-1; i > 0 ; i--) {
                 id = tabsWithSameName.get(i);
-                tp.deleteTabById(id, TasksPage.AlertState.ACCEPT);
+                tl.deleteTabById(id, TaskList.AlertState.ACCEPT);
             }
-            tabsWithSameName = tp.getTabIdListForName(tabName, TasksPage.SearchType.EQUAL);
+            tabsWithSameName = tl.getTabIdListForName(tabName, TaskList.SearchType.EQUAL);
             if(tabsWithSameName.size() == 1)
                 tabId = tabsWithSameName.get(0);
             else
@@ -52,14 +50,16 @@ public class AddAdvancedTaskTest extends BaseTest {
         }
         // if there is no tab with tabName, create a new tab and set its display settings
         else
-            tabId = tp.createNewTab(tabName, TasksPage.AlertState.ACCEPT, "Sort by hand", TasksPage.OptionState.UNSELECT);
-        Assert.assertTrue(tp.isTabExistInVisibleList(tabId), "failed to retrieve a tab for testing!!\n");
+            tabId = tl.createNewTab(tabName, TaskList.AlertState.ACCEPT, "Sort by hand", TaskList.OptionState.UNSELECT);
+        Assert.assertTrue(tl.isTabExistInVisibleList(tabId), "failed to retrieve a tab for testing!!\n");
     }
 
-    @Test
+    @Test(description = "moving to Settings page in order to extract system date formats for following testing methods validations")
     public void tc02_extract_system_date_formats(){
-        tp = new TasksPage(driver);
-        tp.goToSettingsPage();
+        tl = new TaskList(driver);
+        numOfTasksInListBefore = tl.getTasksList().size();
+        totTasksDisplayBefore = tl.getTotalTasksDisplayVal();
+        tl.goToSettingsPage();
         SettingsPage sp = new SettingsPage(driver);
         Assert.assertTrue(sp.isPageDisPlayed(sp.getSettingsLabel()), "Settings page is not displayed!! Cannot retrieve date formats");
         shortDateformat = sp.getShortDateFormat();
@@ -69,54 +69,44 @@ public class AddAdvancedTaskTest extends BaseTest {
         if(shortDateCurrentYearFormat.isEmpty())
             Assert.fail("shortDateCurrentYear format variable is empty");
         sp.goBackToTasksPage();
-        tp = new TasksPage(driver);
-        String expectedPageTitle = tp.getCurrentTabName() + " " + Utils.readProperty("pageTitle");
+        tl = new TaskList(driver);
+        String expectedPageTitle = tl.getCurrentTabName() + " " + Utils.readProperty("pageTitle");
         String actualPageTitle = driver.getTitle();
         Assert.assertEquals(actualPageTitle, expectedPageTitle, "'" + expectedPageTitle + "' page is not displayed");
     }
 
-    @Test
-    public void tc03_go_to_advanced_task_page(){
-        tp = new TasksPage(driver);
-        numOfTasksInListBefore = tp.getTasksList().size();
-        totTasksDisplayBefore = tp.getTotalTasksDisplayVal();
-        tp.goToAdvancedPage();
+    @Test(dataProvider = "getTaskDetails", description = "for each iteration, moving to Advanced Task page, submitting a task, verifying task was added " +
+            "to list and matches the details entered")
+    public void tc03_create_multiple_advanced_tasks(String priorityIn, String dueDateIn, String nameIn, String noteIn, String tagsIn) throws ParseException {
+        tl = new TaskList(driver);
+        if(!tl.getCurrentTabId().equals(tabId))
+            tl.goToTabById(tabId);
+        tl.goToAdvancedPage();
         atp = new AdvancedTaskPage(driver);
-        if(atp.isPageDisPlayed(atp.getNewTaskLabel()))
-            advancedTaskPriorityMap = atp.getPriorityOptionsMap(); // for testing tasks details test method
         Assert.assertTrue(atp.isPageDisPlayed(atp.getNewTaskLabel()), "'Advanced Task' page is not displayed");
-    }
-
-    @Test(dataProvider = "getTaskDetails")
-    public void tc04_create_multiple_advanced_tasks(String priorityIn, String dueDateIn, String nameIn, String noteIn, String tagsIn) throws InterruptedException, ParseException {
-        // check if we are at Tasks page - if so, move to Advanced Task page
-        if(tp.isAdvancedBtnDisplayed()){
-            tp.goToTabById(tabId);
-            tp.goToAdvancedPage();
-        }
-        atp = new AdvancedTaskPage(driver);
+        advancedTaskPriorityMap = atp.getPriorityOptionsMap(); // for testing tasks details test method
         // adding the new advanced task
         enteredTask = new Task(priorityIn, dueDateIn,null,nameIn, noteIn, tagsIn);
         atp.submitTask(enteredTask);
         counter++;
-        tp = new TasksPage(driver);
+        tl = new TaskList(driver);
         // checking if task was added to list - by checking the actual number of tasks in the list
-        int actualNumOfTasksInList = tp.getTasksList().size();
+        int actualNumOfTasksInList = tl.getTasksList().size();
         Assert.assertEquals(actualNumOfTasksInList, numOfTasksInListBefore+counter, "number of tasks in list didn't change after submitting " +
                 "task: " + enteredTask.getTaskName() + "\n");
         // verifying that added task details match the details entered
-        Task addedTask = tp.getTasksList().get(tp.getTasksList().size()-1);
-        Assert.assertTrue(tp.compareTasks(addedTask, enteredTask, TasksPage.ExpectedTaskType.ENTERED, advancedTaskPriorityMap, shortDateformat, shortDateCurrentYearFormat), "one or more task details don't match!! View logs or console messages for more details");
+        Task addedTask = tl.getTasksList().get(tl.getTasksList().size()-1);
+        Assert.assertTrue(tl.compareTasks(addedTask, enteredTask, TaskList.ExpectedTaskType.ENTERED, advancedTaskPriorityMap, shortDateformat, shortDateCurrentYearFormat), "one or more task details don't match!! View logs or console messages for more details");
     }
 
-    @Test
-    public void tc05_check_total_num_tasks_display(){
-        tp = new TasksPage(driver);
+    @Test(description = "verifying tasks total num display after entering multiple advanced tasks")
+    public void tc04_check_total_num_tasks_display(){
+        tl = new TaskList(driver);
         // check if we are the testing tab, if not, move to the testing tab
-        if(!tp.getCurrentTabId().equals(tabId))
-            tp.goToTabById(tabId);
+        if(!tl.getCurrentTabId().equals(tabId))
+            tl.goToTabById(tabId);
         // get actual num of tasks display
-        int actualTotNumOfTaskDisplay = tp.getTotalTasksDisplayVal();
+        int actualTotNumOfTaskDisplay = tl.getTotalTasksDisplayVal();
         Assert.assertEquals(actualTotNumOfTaskDisplay, totTasksDisplayBefore + counter, "Total number of tasks displayed doesn't match " +
                 "the number of tasks in the list");
     }
