cb2c2d5a97d82e9b4b462f12e916c37d2ec24527
rotem <rotem8zaif@gmail.com>
2021-11-26 14:17:34 +0200

    modified tab display settings steps by calling separate functions instead of these action in one function in the framework; add more comments inside the test scenarios methods

--- src/test/java/tests/AddSimpleTaskTest.java
+++ src/test/java/tests/AddSimpleTaskTest.java
@@ -22,39 +22,46 @@ public class AddSimpleTaskTest extends BaseTest {
     public void tc01_create_new_tab_for_testing(){
         tl = new TaskList(driver);
         tabName = "rzf - tasks";
-        List<String> tabsIdWithSameName = tp.getTabIdListForName(tabName, TaskList.SearchType.EQUAL);
         // checking if there are tabs with the same name; if there are, selecting the 1st one and setting its sort display
-        if(tabsIdWithSameName.size() > 0){
+        List<String> tabsIdWithSameName = tl.getTabIdListForName(tabName, TaskList.SearchType.EQUAL);
+        // if the list is not empty then get the first tab
+        if(tabsIdWithSameName.size() > 0)
             tabId = tabsIdWithSameName.get(0);
-            tl.setTabSortDisplay(tabId, TasksPage.SortOption.HAND);
-            tl.setTabCompletedTasksDisplay(tabId, TasksPage.OptionState.UNSELECT);
-        }
         // there are no tabs with name 'rzf - tasks' --> creating a new tab + setting view settings
         else
-            tabId = tl.createNewTab(tabName, TaskList.AlertState.ACCEPT, TasksPage.SortOption.HAND, TasksPage.OptionState.UNSELECT);
+            tabId = tl.createNewTab2(tabName, TaskList.AlertState.ACCEPT);
         tl = new TaskList(driver);
         // verifying tab is created
         if(tabId.isEmpty())
             Assert.fail("failed to create a tab for testing");
-        totalTaskDisplay = tl.getTotalTasksDisplayVal();
+        // set tab sort display and un-select 'Show completed task' option (in case it is selected) for following tests
+        tl.goToTabById(tabId);
+        tl.openTabActionsMenu(tabId);
+        tl.setTabSortDisplay(tabId, TaskList.SortOption.HAND);
+        tl.openTabActionsMenu(tabId);
+        tl.setTabCompletedTasksDisplay(tabId, TaskList.OptionState.UNSELECT);
+        totalTaskDisplay = tl.getTotalTasksDisplayVal(); // getting total tasks display value prior to tasks addition test
         Assert.assertTrue(tl.isTabExistInVisibleList(tabId), "tab id: " + tabId + " is created but not visible\\n");
     }
 
     @Test(dataProvider = "getSimpleTasksData", description = "create multiple simple tasks and verify each new task is added in the list and its name")
-    public void tc03_create_multiple_simple_tasks(String taskName){
+    public void tc02_create_multiple_simple_tasks(String taskName){
         tl = new TaskList(driver);
-        totalTasksInList = tl.getTasksList().size();
+        totalTasksInList = tl.getTaskElementList().size();
         task = new Task(null, null, null, taskName, null, null);
         tl.addNewSimpleTask(task);
+        tl = new TaskList(driver);
         tasksCounter++;
-        int actualTasksInList = tl.getTasksList().size();
+        // checking # of tasks in list after adding a task
+        int actualTasksInList = tl.getTaskElementList().size();
         Assert.assertEquals(actualTasksInList, totalTasksInList+1, "task was not added to list\n");
+        // verifying new task name in the list
         String actualTaskName = tl.getTaskName(tl.getTasksList().size()-1);
         Assert.assertEquals(actualTaskName, taskName, "task was added to list but doesn't have the correct name\n");
     }
 
     @Test(description = "verify total num of tasks display")
-    public void tc04_verify_total_num_of_tasks_display(){
+    public void tc03_verify_total_num_of_tasks_display(){
         tl = new TaskList(driver);
         int actualNumOfTaskDisplay = tl.getTotalTasksDisplayVal();
         Assert.assertEquals(actualNumOfTaskDisplay, totalTaskDisplay + tasksCounter, "total num of tasks displayed doesn't match actual num of tasks in the list");
